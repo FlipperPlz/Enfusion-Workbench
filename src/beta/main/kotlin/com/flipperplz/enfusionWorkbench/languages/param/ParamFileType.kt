7b80fed647f6d25873bc39dfc4c22a521484be88
ryann <ryann@local>
2023-03-20 14:40:20 -0700

    feat(param): Add BraceFolder
    - Brace folder works for enums and arrays

--- src/beta/main/kotlin/com/flipperplz/enfusionWorkbench/languages/param/ParamFileType.kt
+++ src/beta/main/kotlin/com/flipperplz/enfusionWorkbench/languages/param/ParamFileType.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.fileTypes.LanguageFileType
 import javax.swing.Icon
 
 object ParamFileType : LanguageFileType(ParamLanguage){
-    override fun getName(): String = "Param File"
+    override fun getName(): String = "Param"
     override fun getDescription(): String = "A type of configuration file used by Bohemias RV/Enforce engine (Pre-Enfusion)"
     override fun getDefaultExtension(): String = "cpp"
     override fun getIcon(): Icon = AllIcons.FileTypes.Config
